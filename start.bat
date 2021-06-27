248e30cca3152bec809a5df70be7b96c03c69ab4
Muhammad Akbar Habiby Khalid <akbarhabiby@icloud.com>
2021-06-27 20:03:24 +0700

    [Main] Added Windows 11 .NET Framework 3.5 Offline Installer

---
+++ start.bat
@@ -0,0 +1,34 @@
+@echo off
+Title .NET Framework 3.5 Offline Installer for Windows 11
+
+if not "%1"=="am_admin" (
+	echo Asking for administrator permission
+	timeout /t 1
+	powershell start -verb runas '%0' am_admin & exit /b
+)
+
+set pwd=%~dp0
+set dotnet=%pwd%\sources\microsoft-windows-netfx3-ondemand-package~31bf3856ad364e35~amd64~~.cab
+
+if exist %dotnet% (
+	echo Installing .NET Framework 3.5...
+	Dism /online /enable-feature /featurename:NetFX3 /All /Source:%pwd%:\sources /LimitAccess
+	if errorlevel 87 (
+		echo.
+		echo [ERROR] Oopss.. file found but failed install.
+		echo [ERROR] Try to move this folder into C:
+		echo.
+	) else if errorlevel 0 (
+		echo.
+		echo [SUCCESS] .NET Framework 3.5 should be installed
+		echo.	
+	)
+) else (
+	if not errorlevel 87 (
+		echo [ERROR] Oopss.. file not found.
+		echo [ERROR] Please check sources folder and try again.
+		echo.
+	)
+)
+
+pause
